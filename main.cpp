3aa4ed71ca650a7b9e884d212685f04da66a49c2
Furkan Tokac <furkantokac34@gmail.com>
2019-01-30 16:16:32 +0200

    Unnecessary Parts Removed
    Unnecessary parts are removed.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QByteArray>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
         QGuiApplication app(argc, argv);
-
-        QByteArray a("\x00\x00\x00\x00\x00\x00\x00\x00", 8);
-        QByteArray b("\x00\x00\x00\x00\x00\x00\x00\x00", 8);
-
-        a ^= b;
-        qDebug() << a ^ b;
 
         QQmlApplicationEngine engine;
         engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
